fbbd647962e80bebb12893c7794481c6ba2b09ff
Kazuto <mail@kazuto.de>
2023-10-04 12:21:11 +0200

    refactor(ui): extract theme variables as env

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,9 +9,9 @@
     <!-- Styles -->
     <style>
         :root {
-            --color-primary: #FFFFFF;
-            --color-accent: #ff7700;
-            --color-background: #142534;
+            --color-text: {{ config('theme.colors.text') }};
+            --color-accent: {{ config('theme.colors.accent') }};
+            --color-background: {{ config('theme.colors.background') }};
         }
     </style>
     @vite('resources/css/app.css')
