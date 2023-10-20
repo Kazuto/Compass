f8388761a98ec2e031b7128e813f216e3e5de62f
Kazuto <mail@kazuto.de>
2023-10-20 11:20:01 +0200

    fix(bookmark): show question icon if invalid icon name

--- resources/views/components/bookmark/item.blade.php
+++ resources/views/components/bookmark/item.blade.php
@@ -1,8 +1,6 @@
 <li>
     <a class="inline-flex items-center gap-1 text-sm transition-all hover:translate-x-2" href="{{ $bookmark->url }}">
-        @if(filled($bookmark->icon))
-            @svg($bookmark->icon, ['class' => 'h-3 w-3'])
-        @endif
+        {{ $bookmark->svgIcon('h-3 w-3') }}
         {{ $bookmark->name }}
     </a>
 </li>
